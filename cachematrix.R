89873c1367618e15f3be442eb488425277eb14a7
jasonwinbolt <jasonwinbolt@yahoo.com>
2014-04-26 22:49:00 -0500

    Changes to Prog Assignment 2
    updated as per instructions.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions create a special object that stores a square matrix and caches the inverse of that matrix.
+## 
 
-## Write a short comment describing this function
+## This function creates square matrix in order to set and get the matrix and then set and get the inverse matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) i <<- inverse
+  getinverse <- function() i
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+  
 }
 
 
-## Write a short comment describing this function
+## This function calculates the invers of the special matrix in the first function.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    i<-x$getinverse()
+    if(!is.null(i)){
+          message("getting cached data")
+          return(i)
+    }
+    data<-x$get()
+    i<-inverse(data, ...)
+    x$setinverse(i)
+    i
 }
